3a580fe22691325df1fa68c5e9a7ab0ebf687e5e
raohai.rh <raohai.rh@alibaba-inc.com>
2020-07-18 23:07:25 +0800

    feat: add github client

--- .functions/webhook.js
+++ .functions/webhook.js
@@ -1,4 +1,13 @@
-exports.handler = function(event, context, callback) {
-    console.log('>> webhook received', event);
-    callback(null, { statusCode: 200, body: "Hello, World" });
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit({
+  auth: process.env.GITHUB_SECRET_FOR_NETLIFY,
+});
+
+exports.handler = async function(event, context, callback) {
+    console.log('>> webhook received', process.env.GITHUB_SECRET_FOR_NETLIFY);
+
+    callback(null, {
+      statusCode: 200,
+      body: await octokit.request('/users'),
+    });
 }
