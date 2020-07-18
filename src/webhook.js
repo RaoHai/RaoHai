2df1eae2520867d7b2a49fdf4c8f4e6962df44fe
raohai.rh <raohai.rh@alibaba-inc.com>
2020-07-19 00:48:54 +0800

    feat: detect post

--- src/webhook.js
+++ src/webhook.js
@@ -5,6 +5,16 @@ const octokit = new Octokit({
 
 exports.handler = async function(event, context, callback) {
 
+  if (event.httpMethod !== 'POST') {
+    console.log('Invalid httpMethod:', event.httpMethod);
+
+    callback(null, {
+      statusCode: 200,
+      body: 'Invalid httpMethod',
+    });
+  }
+
+  console.log('>> event received', event);
   const result = await octokit.actions.createWorkflowDispatch({
     owner: 'RaoHai',
     repo: 'RaoHai',
