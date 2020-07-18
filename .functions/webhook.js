7ea28aa36c5d881e3889cc7cb82f67a615c2acc5
陆沉 <surgesoft@gmail.com>
2020-07-18 22:44:44 +0800

    Update webhook.js

--- .functions/webhook.js
+++ .functions/webhook.js
@@ -1,6 +1,4 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
-    });
+    console.log('>> webhook received', event);
+    callback(null, { statusCode: 200, body: "Hello, World" });
 }
